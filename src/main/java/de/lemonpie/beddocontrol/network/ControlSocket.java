71c3c69c4ae46a1efeee8d339216521c48882840
tobias <thinkdifferent055@gmail.com>
2017-08-21 18:16:27 +0200

    Add client socket handler

---
+++ src/main/java/de/lemonpie/beddocontrol/network/ControlSocket.java
@@ -0,0 +1,94 @@
+package de.lemonpie.beddocontrol.network;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class ControlSocket implements Runnable {
+
+	private static final int MAX = 10;
+
+	private String host;
+	private int port;
+
+	private Socket socket;
+	private BufferedReader inputStream;
+	private PrintWriter outputStream;
+
+	private Thread readerThread;
+
+	public ControlSocket(String host, int port) {
+		this.host = host;
+		this.port = port;
+	}
+
+	public boolean connect() {
+		int counter = 0;
+		while (counter < MAX) {
+			try {
+				initConnection();
+				break;
+			} catch (IOException e) {
+				e.printStackTrace();
+				try {
+					Thread.sleep(5 * 1000);
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+					break;
+				}
+			}
+			counter++;
+		}
+		return socket.isConnected();
+	}
+
+	private void initConnection() throws IOException {
+		socket = new Socket();
+		socket.connect(new InetSocketAddress(host, port));
+
+		inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		outputStream = new PrintWriter(socket.getOutputStream(), true);
+
+		readerThread = new Thread(this);
+		readerThread.start();
+	}
+
+	public void interrupt() {
+		readerThread.interrupt();
+	}
+
+	public void close() throws IOException {
+		inputStream.close();
+		outputStream.close();
+		socket.close();
+	}
+
+	public void write(String data) {
+		outputStream.println(data); // AutoFlush is enable
+	}
+
+	@Override
+	public void run() {
+		try {
+			String line;
+			while ((line = inputStream.readLine()) != null) {
+				System.out.println(line);
+
+				if (Thread.interrupted()) {
+					break;
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
