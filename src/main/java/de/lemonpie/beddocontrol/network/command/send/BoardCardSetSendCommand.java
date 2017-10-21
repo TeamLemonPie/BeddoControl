e3ea76a75c361a83ad3f793edf51a632ba7f2690
tobias <thinkdifferent055@gmail.com>
2017-10-21 22:08:49 +0200

    Add board card set command

---
+++ src/main/java/de/lemonpie/beddocontrol/network/command/send/BoardCardSetSendCommand.java
@@ -0,0 +1,15 @@
+package de.lemonpie.beddocontrol.network.command.send;
+
+import com.google.gson.JsonPrimitive;
+import de.lemonpie.beddocontrol.model.card.Card;
+import de.lemonpie.beddocontrol.network.CommandName;
+import de.lemonpie.beddocontrol.network.ControlCommandData;
+import de.lemonpie.beddocontrol.network.Scope;
+
+public class BoardCardSetSendCommand extends ControlCommandData {
+
+	public BoardCardSetSendCommand(int index, Card card) {
+		super(Scope.ADMIN, CommandName.BOARD_CARD, index);
+		setValue(new JsonPrimitive(card.getName()));
+	}
+}
