698515071afa719494ecaad7276b127a111cd116
tobias <thinkdifferent055@gmail.com>
2018-08-10 23:19:21 +0200

    Improve BlindController (#23)

--- src/main/java/de/lemonpie/beddocontrol/ui/BlindController.java
+++ src/main/java/de/lemonpie/beddocontrol/ui/BlindController.java
@@ -3,8 +3,6 @@ package de.lemonpie.beddocontrol.ui;
 import de.lemonpie.beddocontrol.model.Board;
 import de.lemonpie.beddocontrol.network.ControlSocket;
 import de.lemonpie.beddocontrol.network.command.send.AnteSendCommand;
-import de.lemonpie.beddocontrol.network.command.send.BigBlindSendCommand;
-import de.lemonpie.beddocontrol.network.command.send.SmallBlindSendCommand;
 import de.tobias.utils.nui.NVC;
 import fontAwesome.FontIcon;
 import fontAwesome.FontIconType;
@@ -22,6 +20,12 @@ import java.net.SocketException;
 
 public class BlindController extends NVC
 {
+
+	private interface ValueHandler
+	{
+		void handle(int value) throws SocketException;
+	}
+
 	@FXML
 	private TextField textFieldSmallBlind;
 	@FXML
@@ -78,81 +82,52 @@ public class BlindController extends NVC
 	@FXML
 	public void setSmallBlind()
 	{
-		String smallBlindText = textFieldSmallBlind.getText().trim();
-		if(smallBlindText == null || smallBlindText.equals(""))
-		{
-			AlertGenerator.showAlert(Alert.AlertType.WARNING, "Warning", "", "Please enter a small blind value", ImageHandler.getIcon(), getContainingWindow(), null, false);
-			textFieldSmallBlind.setStyle("-fx-border-color: #CC0000; -fx-border-width: 2");
-			return;
-		}
-
-		final int smallBlind = Integer.parseInt(smallBlindText);
-		try
-		{
-			board.setSmallBlind(smallBlind);
-			socket.write(new SmallBlindSendCommand(smallBlind));
-		}
-		catch(SocketException e)
-		{
-			Logger.error(e);
-			textFieldSmallBlind.setStyle("-fx-border-color: #CC0000; -fx-border-width: 2");
-			AlertGenerator.showAlert(Alert.AlertType.ERROR, "Error", "An error occurred", e.getMessage(), ImageHandler.getIcon(), getContainingWindow(), null, false);
-		}
-
-		textFieldSmallBlind.setStyle("-fx-border-color: #48DB5E; -fx-border-width: 2");
+		set(textFieldAnte, "a small blind", value -> {
+			board.setAnte(value);
+			socket.write(new AnteSendCommand(value));
+		});
 	}
 
 	@FXML
 	public void setBigBlind()
 	{
-		String bigBlindText = textFieldBigBlind.getText().trim();
-		if(bigBlindText == null || bigBlindText.equals(""))
-		{
-			AlertGenerator.showAlert(Alert.AlertType.WARNING, "Warning", "", "Please enter a big blind value", ImageHandler.getIcon(), getContainingWindow(), null, false);
-			textFieldBigBlind.setStyle("-fx-border-color: #CC0000; -fx-border-width: 2");
-			return;
-		}
-
-		final int bigBlind = Integer.parseInt(bigBlindText);
-		try
-		{
-			board.setBigBlind(bigBlind);
-			socket.write(new BigBlindSendCommand(bigBlind));
-		}
-		catch(SocketException e)
-		{
-			Logger.error(e);
-			textFieldBigBlind.setStyle("-fx-border-color: #CC0000; -fx-border-width: 2");
-			AlertGenerator.showAlert(Alert.AlertType.ERROR, "Error", "An error occurred", e.getMessage(), ImageHandler.getIcon(), getContainingWindow(), null, false);
-		}
-
-		textFieldBigBlind.setStyle("-fx-border-color: #48DB5E; -fx-border-width: 2");
+		set(textFieldAnte, "a big blind", value -> {
+			board.setAnte(value);
+			socket.write(new AnteSendCommand(value));
+		});
 	}
 
 	@FXML
 	public void setAnte()
 	{
-		String anteText = textFieldAnte.getText().trim();
-		if(anteText == null || anteText.equals(""))
+		set(textFieldAnte, "an ante", value -> {
+			board.setAnte(value);
+			socket.write(new AnteSendCommand(value));
+		});
+	}
+
+	private void set(TextField textField, String textPart, ValueHandler handler)
+	{
+		textField.setStyle("-fx-border-color: #CC0000; -fx-border-width: 2");
+		String valueText = textField.getText().trim();
+
+		if(valueText.equals(""))
 		{
-			AlertGenerator.showAlert(Alert.AlertType.WARNING, "Warning", "", "Please enter an ante value", ImageHandler.getIcon(), getContainingWindow(), null, false);
-			textFieldAnte.setStyle("-fx-border-color: #CC0000; -fx-border-width: 2");
+			AlertGenerator.showAlert(Alert.AlertType.WARNING, "Warning", "", "Please enter " + textPart + " value", ImageHandler.getIcon(), getContainingWindow(), null, false);
 			return;
 		}
 
-		final int ante = Integer.parseInt(anteText);
+		final int value = Integer.parseInt(valueText);
 		try
 		{
-			board.setAnte(ante);
-			socket.write(new AnteSendCommand(ante));
+			handler.handle(value);
+			textField.setStyle("-fx-border-color: #48DB5E; -fx-border-width: 2");
 		}
 		catch(SocketException e)
 		{
 			Logger.error(e);
-			textFieldAnte.setStyle("-fx-border-color: #CC0000; -fx-border-width: 2");
 			AlertGenerator.showAlert(Alert.AlertType.ERROR, "Error", "An error occurred", e.getMessage(), ImageHandler.getIcon(), getContainingWindow(), null, false);
 		}
 
-		textFieldAnte.setStyle("-fx-border-color: #48DB5E; -fx-border-width: 2");
 	}
 }
